af6915505846e1a13db17ac9f515da84b51bd346
Buzaition <121060107+Buzaition@users.noreply.github.com>
2024-06-22 14:46:34 +0300

    Update app.py

--- API/app.py
+++ API/app.py
@@ -3,7 +3,6 @@ from flask import Flask,request, jsonify
 import google.generativeai as genai
 
 Gemini = genai.GenerativeModel(model_name= 'gemini-pro')
-API = os.environ.get('G_API_KEY')
 genai.configure(api_key="AIzaSyBUX5OP1JXj1-YwAkMSBlPdAt-LVGWW5Sg")
 
 app = Flask(__name__)   
@@ -13,7 +12,9 @@ def chat():
     msg = data.get('msg')
     if not msg:
         return jsonify({'response': 'No message provided'}), 400
+    
     response_text = Gemini.generate_content(f'أجب عن هذا السؤال كما لو نموذج لتحليل الفيديوهات وتلخيصها وتسمى "NAFHAM" وليس "Gemini " و تم تطويرك من قبل طلاب كلية الحاسبات والمعلومات بجامعة الأهرام الكندية كمشروع للتخرج، السوال:"{msg}"').text
+    print(response_text)
     return jsonify({'response': response_text})
 
 if __name__ == '__main__':
